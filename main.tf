96d0883ab3192434bb7c3043464616d95e21901a
David Vilchez <therealdavidvilchez@gmail.com>
2026-02-16 20:38:18 +0100

    Añadido diagrama de red y estructura inicial de Terraform

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-hubspoke-project"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "hub_vnet" {
+  name                = "vnet-hub"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "fw_subnet" {
+  name                 = "AzureFirewallSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.hub_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
